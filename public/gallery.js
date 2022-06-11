1f604c4eeedd34bc52993eef52e145bdff1a2a53
ZachMuzzle <zach12342@me.com>
2022-06-11 12:08:29 -0400

    Added model image click. Needed to still format css for gallery page better.

--- public/gallery.js
+++ public/gallery.js
@@ -1,15 +1,50 @@
+try {
+    window.onload = function displayImage() {
 
-window.onload = function displayImage() {
-    const dataUrl = sessionStorage.getItem("url-dino");
-    const dataAlt = sessionStorage.getItem("dino-name");
-    const urlArray = dataUrl.split(',');
-    const nameArray = dataAlt.split(',');
+        const dataUrl = sessionStorage.getItem("url-dino");
+        const dataAlt = sessionStorage.getItem("dino-name");
+        if(dataUrl == null) throw "Generate a dino image!"
+        if(dataAlt == null) throw "Generate a dino name!";
+        const urlArray = dataUrl.split(',');
+        const nameArray = dataAlt.split(','); 
 
-    for(let i = 0; i < urlArray.length; i++) {
-    let img = document.createElement('img');
-    img.className = 'dinoGalleryImage';
-    img.src = urlArray[i];
-    img.alt = nameArray[i];
-    document.querySelector('#dinoWrapperGallery').appendChild(img);
+        for(let i = 0; i < urlArray.length; i++) {
+        let img = document.createElement('img');
+        img.className = 'dinoGalleryImage';
+        img.src = urlArray[i];
+        img.alt = nameArray[i];
+        document.querySelector('#dinoWrapperGallery').appendChild(img);
+        }
+
+         /* MODEL FOR IMAGE */
+         let model = document.getElementById("myModel");
+         let images = document.getElementsByClassName("dinoGalleryImage");
+         let imgSrc = document.getElementById("img-src");
+         let imgCaption = document.getElementById("caption");
+         /** 
+          * * We create the model after  we have loaded the images onto the page since they are local to the loop
+          * ? There may be a way to do this outside of  this function, but I thought this was the easiest to implement
+          * 
+          * */ 
+         try {
+            if(images.length == 0) throw "No images found!";
+            for(let j =  0; j < images.length; j++) {
+                let modelImg = images[j];
+            modelImg.onclick = function(event) {
+                model.style.display = "block";
+                imgSrc.src = this.src;
+                imgCaption.innerHTML = this.alt;
+            }
+            }
+         } catch(err) {
+            console.log(err);
+            console.log("Length of images: " + images.length);
+         }
+         let span = document.getElementsByClassName("close")[0];
+         span.onclick = function() {
+            model.style.display = "none";
+         }
     }
+} catch(err) {
+    console.log(err);
 }
