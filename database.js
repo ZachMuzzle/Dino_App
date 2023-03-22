7d89f4891145a828177f0995b7473a9502d5fc5a
ZachMuzzle <zach12342@me.com>
2023-03-22 17:12:57 -0400

    Added insert and getAll option for database functionality. Pictures now display from database on webpage.

--- database.js
+++ database.js
@@ -1,4 +1,6 @@
-var mysql = require('mysql');
+// var mysql = require('mysql');
+import mysql from 'mysql'
+let instance = null;
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -10,4 +12,50 @@ connection.connect(function(err) {
     if(err) throw err;
     console.log('Database is connected successfully!')
 });
-module.exports = connection;
+// module.exports = connection;
+
+export default class DbService {
+    static getDbServiceInstance() {
+        return instance ? instance : new DbService();
+    }
+
+    async getAllData() {
+        try {
+            const response = await new Promise((resolve, reject) => {
+                const query = "SELECT * FROM dino_table";
+
+                connection.query(query, (err, results) => {
+                    if(err) reject(new Error(err.message));
+                    resolve(results);
+                })
+            });
+            return response; 
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+    async insertDino(dino_name,dino_image_url) {
+        try {
+            const dateAdded = new Date();
+            const insertId = await new Promise((resolve, reject) => {
+                const query = "INSERT INTO dino_table (dino_name, dino_image_url, date_added) VALUES (?,?,?);";
+                connection.query(query, [dino_name,dino_image_url,dateAdded], (err, result) => {
+                    if(err) reject(new Error(err.message));
+                    resolve(result.insertId)
+                })
+            });
+            console.log(insertId)
+            return {
+                id: insertId,
+                dino_name: dino_name,
+                dino_image_url: dino_image_url,
+                dateAdded: dateAdded
+            }
+        } catch(error) {
+            console.log(error);
+        }
+    }
+}
+
+// module.exports =  DbService;
