4142be47c3eb42ba25de562c0c05e3cd7de2c7e4
ZachMuzzle <zach12342@me.com>
2024-04-01 17:19:25 -0400

    Added css for mobile responsive design. Also removed error message if submit is click when recaptcha is completed

--- public/Recaptcha.js
+++ public/Recaptcha.js
@@ -15,6 +15,8 @@ loginForm.addEventListener('submit', async function submitForm(e) {
     //     alert("Please fill out both Email and Password fields")
     // } else {
     await submitClick()
+    /* Below only continues if no errors occur in submitClick() */
+    document.getElementById('popupMessage').style.display = 'none';
     loginModel.style.display = "none"
     /*
     !! Web Browser freezes when using alert  
@@ -55,9 +57,6 @@ function popupMessageAlert() {
         // console.log(captchaResponse)
         if (!captchaResponse.length > 0) {
             if(document.getElementById("popupValue")) {
-                /*
-                * Add shake effect to element
-                */
                addShakeEffect();
                setTimeout(removeShakeEffect,2000);
                 throw new Error('Complete the Captcha!')
